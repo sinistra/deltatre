b6f6d43e27bb16cb8968c50f87b82cf0e2363e99
Paul Taylor <paul@nms.com.au>
2020-01-22 15:43:17 +1100

    wip

--- frontend/controllers/word.go
+++ frontend/controllers/word.go
@@ -1,12 +1,33 @@
 package controllers
 
 import (
+	"net/http"
+
+	"github.com/davecgh/go-spew/spew"
 	"github.com/gin-gonic/gin"
+	"sinistra/deltatre/frontend/services"
 )
 
 type WordController struct{}
 
 func (w WordController) GetWord(ctx *gin.Context) {
+	spew.Dump(ctx.Request.URL.Query())
+	// name := ctx.Query("name")
+	word := ctx.Param("word")
+
+	service := services.WordService{}
+
+	result, err := service.GetWord(word)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Server error", "error": err})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+		"data":    result,
+	})
 }
 
 func (w WordController) AddWord(ctx *gin.Context) {
--- frontend/services/word_http.go
+++ frontend/services/word_http.go
@@ -1,7 +1,13 @@
 package services
 
 import (
-	"../../models"
+	"bytes"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sinistra/deltatre/models"
+	"time"
 )
 
 type WordService struct{}
@@ -22,4 +28,36 @@ func (w WordService) TopWords(count int) ([]models.Word, error) {
 	var words []models.Word
 
 	return words, nil
+}
+
+func httpRequest(method, url string, body []byte) ([]byte, error) {
+	var bodyBuffer io.Reader
+	if len(body) > 0 {
+		bodyBuffer = bytes.NewBuffer(body)
+	}
+
+	client := http.Client{
+		Timeout: time.Second * 30,
+	}
+	request, err := http.NewRequest(method, url, bodyBuffer)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	request.Header.Set("Content-Type", "application/vnd.api+json")
+	request.Header.Set("x-api-key", "ITG.9248ce9dc72946701df7119f02f9ca1e.Xo_Qgyy6ER1uJkAKpCoUo-uvSiXHgD6vtQFh0X1MUmpp35gtdMco7BnEkB3d_726")
+
+	response, err := client.Do(request)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer response.Body.Close()
+	body, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return body, nil
 }
